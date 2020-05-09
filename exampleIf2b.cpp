a4e5bde86cc17915bbc89ea49aa5ac9076ff9146
Delenn <40397437+delennb@users.noreply.github.com>
2020-05-09 16:19:43 -0400

    Part B of second if example

---
+++ exampleIf2b.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 7;
+    
+    if (a>5)
+        cout << "a is greater than 5" << endl;
+    
+    cout << "Done" << endl;
+    
+    return 0;
+}
