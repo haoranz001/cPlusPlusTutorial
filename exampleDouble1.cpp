8827435b0d57d7559e4d456f62c1a994c14dba90
Haoran Zhang <haoranz@umich.edu>
2020-05-09 10:09:46 -0400

    more things

---
+++ exampleDouble1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double a = 1;
+	double b = 2;
+	cout << a / b << endl;
+	return 0; // This is a sanity check.
+	// By default, you want C++ to return 0 if the program runs without error.
+}
---
+++ exampleInt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 1;
+	int b = 2;
+	cout << a/b << endl;
+	return 0; // This is a sanity check.
+	// By default, you want C++ to return 0 if the program runs without error.
+}
